a610f0f4e7f0389e528508c69b5e2f722ddf5907
Bryan Burch <bburch@csus.edu>
2021-11-11 09:47:07 -0800

    Vehicle class

---
+++ DreamTeam_FinalProject.java/src/Vehicle.java
@@ -0,0 +1,23 @@
+
+public class Vehicle {
+	private int    year;
+	private String make;
+	private String model;
+	private String license;
+	private String vin;
+	
+	public Vehicle(int year, String make, String model, String license, String vin) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.license = license;
+		this.vin = vin;
+	}
+	
+	public String getVehicleInfo() {
+		String info = year + " " + make + " " + model
+					  + "\nLicense Plate: " + license
+					  + "\nVIN: " + vin;
+		return info;
+	}
+}
