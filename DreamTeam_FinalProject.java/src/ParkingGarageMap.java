de6452ffdb761131ce7bb4b7e1ab506f65992f68
pprabhu2727 <pprabhu@csus.edu>
2021-11-17 11:32:23 -0800

    Changes to createMaps() method

--- DreamTeam_FinalProject.java/src/ParkingGarageMap.java
+++ DreamTeam_FinalProject.java/src/ParkingGarageMap.java
@@ -19,19 +19,23 @@ public class ParkingGarageMap {
 			{ '*', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', '@' },
 			};
 	
-	/*Thinking about creating an array of 2d maps for each floor below*/
+	/*Creates a boolean array of the availability of each slot in order for each floor of each garage*/
 	public void createMaps() {
-		HashMap<String, char[][]> garageMaps = new HashMap<String, char[][]>();  //Key format is "#:#" where you have GarageNumber:FloorNumber
+		HashMap<String, boolean[]> garageMaps = new HashMap<String, boolean[]>();  //Key format is "#:#" where you have GarageNumber:FloorNumber
 		ArrayList<ParkingGarage> garageList = CampusParking.getGarageList();
+		int numberOSlots = garageList.get(0).getFloorList().get(0).getNumberOfSlots();
+		
 		
 		for(int i=1; i<=garageList.size(); i++) {
 			for(int j=1; j<=garageList.get(0).getNumberOfFloors();j++) {
 				String key = i + ":" + j;
-				garageMaps.put(key, new char[j][garageList.get(0).getFloorList().get(0).getNumberOfSlots()]);
+				boolean[] slotAvailabilityList = new boolean[numberOSlots];
+				for(int k=0; k<numberOSlots; k++) {
+					slotAvailabilityList[k] = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getAvailability();
+				}
+				garageMaps.put(key, slotAvailabilityList);
 			}
 		}
-		//TODO: Use the ParkingSlot class to fill in the 2d char arrays for each floor. 
-
 	}
 	
 	
