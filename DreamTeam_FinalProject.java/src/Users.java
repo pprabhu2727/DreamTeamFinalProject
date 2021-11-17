7e86b9fd868be632d1e1b120fd089a7380d60818
Rthandi01 <91033806+Rthandi01@users.noreply.github.com>
2021-11-16 19:56:00 -0800

    Update Users.java
    Added Register

--- DreamTeam_FinalProject.java/src/Users.java
+++ DreamTeam_FinalProject.java/src/Users.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.StackWalker.Option;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
@@ -19,9 +20,9 @@ public class Users{
 	private String Address;
 	private String Username;
 	private String Password;
-	private long PhoneNumber;
+	private String PhoneNumber;
 	HashMap <String,String> Login = new HashMap<String,String>();
-	public Users(String name, String Email, String Address, String Username, String Password, long PhoneNumber) {
+	public Users(String name, String Email, String Address, String Username, String Password, String PhoneNumber) {
 		this.name = name;
 		this.Email = Email;
 		this.Address = Address;
@@ -62,10 +63,10 @@ public class Users{
 	public String getUsername() {
 		return this.Username;
 	}
-	public void setPhone(long PhoneNumber) {
+	public void setPhone(String PhoneNumber) {
 		this.PhoneNumber = PhoneNumber;
 	}
-	public long getPhone() {
+	public String getPhone() {
 		return this.PhoneNumber;
 	}
 	public void setPassword(String Password) {
@@ -74,14 +75,8 @@ public class Users{
 	public String getPassword() {
 		return this.Password;
 	}
-	public void IDPass(){
-		Login.put("Rayman","Thandi");
-
-
-	}
-	public HashMap Info(){
-
-		return Login;
+	public String toString(){
+		return "User Info: Name: " + this.name + "\n Email \n" + this.name + " ";
 	}
 	
 }
@@ -100,15 +95,30 @@ public class Users{
         // Create an empty list to hold users
         ArrayList<Users> listOfUsers = new ArrayList<Users>();
         
-        // Add 3 users to the list
-        listOfUsers.add(new Users("Rayman","Thandi"));
+        // Add users to the list
+        listOfUsers.add(new Users("Rayman", "email@email.com","Address","ThandiR","password","9166666666"));
         listOfUsers.add(new Users("Test","Password"));
         listOfUsers.add(new Users("Rayman","Thandi"));
-        // Get user input
+      
+      
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Scanner s = new Scanner(System.in);
+		System.out.println(greeting);
+		
+		int option = -1;
+	
+		do{
 
+            System.out.println("Enter 1 to login");
+            System.out.println("Enter 2 to Resigter");
+			System.out.println("Enter 3 to Show Information");
+			System.out.println("Enter 3 to Quit");
+            option = s.nextInt();
+
+    
+	switch(option){
+       case 1:
         
-        System.out.println(greeting);
         System.out.println("Please type your username :");
         username = br.readLine();   
         System.out.println("Please type your password :");
@@ -133,13 +143,45 @@ public class Users{
         // if loggedInUser was changed from null, it was successful
         if (loggedInUser != null)
         {
-            System.out.println("User successfully logged in: "+loggedInUser.getUsername());
+            System.out.println("User successfully logged in: "+loggedInUser.getName());
         }
         else
         {
-            System.out.println("Invalid username/password combination");
+            System.out.println("Invalid username/password");
         }
-    }
+			break;
+		case 2:
+			Users users = new Users();
+			Scanner scanner = new Scanner(System.in);
+				System.out.print(" Enter Name ");
+				String Name = scanner.nextLine();
+				users.setName(Name);
+	
+				System.out.print(" Enter userName ");
+				username = scanner.nextLine();
+				users.setUsername(username);
+	
+				System.out.print(" Enter password => ");
+				password = scanner.nextLine();
+				users.setPassword(password);
+	
+				System.out.print(" Enter emailId => ");
+				String emailId = scanner.nextLine();
+				users.setEmail(emailId);
+	
+				System.out.print(" Enter phoneNo => ");
+				String phoneNo = scanner.nextLine();
+				users.setPhone(phoneNo);
+			
+				listOfUsers.add(new Users(Name, emailId, emailId, username, password, phoneNo));
+		case 3:
+		Users user = new Users();
+		System.out.print(user.toString());
+		break;
 
 }
+		}while(option !=4);
+	}
+}
+
 
