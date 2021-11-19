14ea590ef063eb372c50dfed6e95199d90d83dc6
slivb <slivb@10.0.0.86>
2021-11-19 08:32:01 -0800

    Started permit class. The idea is to check if user has valid permit.

---
+++ DreamTeam_FinalProject.java/src/Permit.java
@@ -0,0 +1,27 @@
+
+public class Permit {
+
+	private String permit;
+	private boolean b;
+	
+	public Permit(String permit){
+		this.permit = permit;
+	}
+	
+	public void setPermit(String permit){
+		this.permit = permit;
+	}
+	
+	public String getPermit(){
+		return this.permit;
+	}
+	
+	public boolean checkPermit(){
+		if(permit == null)
+			b = false;
+		else
+			b = true;
+		
+		return b;
+	}
+}
