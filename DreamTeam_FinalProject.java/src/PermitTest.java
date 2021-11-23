1710ac590b19e3193bcd23449982e66dc943221a
slivb <slivb@10.0.0.86>
2021-11-23 12:29:15 -0800

    Added a test for Permit

---
+++ DreamTeam_FinalProject.java/src/PermitTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PermitTest {
+
+	@Test
+	public void test() {
+		boolean b;
+		String s = null;
+		
+		Permit p = new Permit(s);
+		
+		b = p.checkPermit();
+		
+		assertEquals(false, b);
+	}
+
+}
