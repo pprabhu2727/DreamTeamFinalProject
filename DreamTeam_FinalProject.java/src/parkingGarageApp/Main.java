68180295ac91552a290e0bd16cc61abbfe2f6843
Rthandi01 <91033806+Rthandi01@users.noreply.github.com>
2021-11-24 12:58:17 -0800

    Update Main.java

--- DreamTeam_FinalProject.java/src/parkingGarageApp/Main.java
+++ DreamTeam_FinalProject.java/src/parkingGarageApp/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO: Initialization tasks 
 		CampusParking.createInstance();
 		
