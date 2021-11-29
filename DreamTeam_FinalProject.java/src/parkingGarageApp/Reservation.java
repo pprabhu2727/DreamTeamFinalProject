dd7ad1d52cc30b112b5e0b42588f78bf234a718a
Whonald <ronaldher2@csus.edu>
2021-11-28 16:54:23 -0800

    Reservation
    Implemented users+Garage classes into reservation

--- DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
+++ DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
@@ -8,15 +8,114 @@ import org.junit.rules.ExpectedExceptions;
 
 public class Reservation {
 	
-	Reservation reserve;
-	
-	//testing to see if created
-	public void test() {
-		reserve = new Reservation();
-	}
-	
 	static Scanner key = new Scanner(System.in);
 	static Scanner o = new Scanner(System.in);
+	static Scanner g = new Scanner(System.in);
+	static Scanner f = new Scanner(System.in);
+	static Scanner s = new Scanner(System.in);
+	private Users currentUser;
+	private Users getReservation;
+	private Users getName;
+	private ParkingGarage getSlotNumber;
+	private ParkingGarage getGarageNumber;
+	private ParkingGarage getFloorNumber;
+	private ParkingSlot getAvaSlot;
+	int choice, gChoice, fChoice, sChoice;
+
+			public Reservation(int garageNumber, int floorNumber, int slotNumber)
+			{
+				
+				System.out.println(" Option 1 to reserve");
+				System.out.println(" Option 2 to cancel reservation");
+				System.out.println(" Option 3 to do a quick reservation");
+				System.out.println(" Option 4 to Exit");
+
+				choice = o.nextInt();
+
+				do{
+					switch(choice){
+						case 1:
+
+							System.out.println(" Please enter a garage number you'd like to reserve from: ");
+							gChoice = g.nextInt();
+							System.out.println(" Please enter a floor number you'd like to reserve from: ");
+							fChoice = f.nextInt();
+							System.out.println(" Please enter a slot number you'd like to reserve: ");
+							sChoice = s.nextInt();
+							ParkingSlot.ParkingSlot(gChoice, fChoice, sChoice);	//Is there a way to change the method to work like this? and make getAvailability to check exact spot?
+
+							if(ParkingSlot.getAvailability == true)
+							{
+								ParkingSlot.changeAvailability;
+								ParkingSlot.setUser;
+							}
+				
+							break;
+
+						case 2:
+							//Cancel reservation through spot location?
+								String name, int floorNumber, slotNumber;
+								while (input.hasNext() && !found){
+							try {
+								if (information.equals(name)) && (information.equals(floorNumber) && (informatiion.equals(slotNumber)))
+								{
+									found = true;
+								}
+							}
+							System.out.println ("Cancelation successful")
+							}catch(Exception e){
+
+								System.out.println( "Cancelation unsuccessful");
+							}
+
+							break;
+
+						case 3:
+							//Quick Reserve. (Pseudo code that needs to be edited after the previous errors in this class are fixed)
+							
+							int garageNumber, floorNumber, slotNumber;
+							boolean foundSlot = false;
+							while(!foundSlot){
+									for(int i=1; i<=garageList.size(); i++) {
+										for(int j=1; j<=garageList.get(0).getNumberOfFloors();j++) {			
+											for(int k=0; k<numberOSlots; k++) {
+												boolean slotAvailability = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getAvailability();
+												if (slotAvailability){
+													slotNumber = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getSlotNumber();
+													garageNumber=i;
+													floorNumber=j;
+													foundSlot=true;
+
+												}
+												if(foundSlot){
+													
+													break;
+												}
+											}
+											if(foundSlot){
+													break;
+											}
+										}
+										if(foundSlot){
+													break;
+										}
+									}
+									
+									//Now do the reservation with garageNumber, floorNumber, slotNumber
+									
+							}
+						
+						
+						//default:
+							
+				
+				
+			}
+		
+		}while(choice != 4);
+	}
+
+}
 	
 /*
 	public void checkSpot() {
@@ -36,130 +135,39 @@ public class Reservation {
 			System.out.println("Spot is currently filled by, " + spotName);			//loop back?
 		}
 	}
-*/
-	
-	
-	
-	
-	int choice;
 
-		System.out.println(" Option 1 to reserve");
-		System.out.println(" Option 2 to cancel reservation");
-		System.out.println(" Option 3 to do a quick reservation");
-		System.out.println(" Option 4 to Exit");
-
-		choice = o.nextInt();
-
-		do{
-			switch(choice){
-				case 1:
-
-				@SuppressWarnings("unused")	//I have no clue affects spotNumber
-				public void reserveSpot() throws IOException{
+	public class information {
+								private String Name;
+								private double floorNumber;
+								private String spotName;
+								private double timeNumber;
+								private double timeNumber2;
 					
-					//System.out.println("Input the time range that you would like to book for. (Military Time) \n Example: 12.30(enter), 16.30(enter)");
-					//double timeNumber = key.nextDouble();
-					//double timeNumber2 = key.nextDouble();
-					System.out.println("What name would you like to have this spot named under? ");
-					String spotName = key.toString();
-					System.out.println("Enter in Floor Number");
-					double floorNumber = key.nextDouble();
+								public void information (String Name, double floorNumber, double timeNumber, double timeNumber2)
+								{
+									this.Name = Name;
+									this.floorNumber = floorNumber;
+									this.timeNumber = timeNumber;
+									this.timeNumber2 = timeNumber2;
+								}
+						public information() {
 					
-					
-					reserve.reserveParkingSpot(spotName, floorNumber, spotNumber, timeNumber, timeNumber2);
-					System.out.println("The spot you have choosen has now been booked from, " + timeNumber + " - " + timeNumber2 + ".\n");
-				}
-				public class information {
-						private String Name;
-						private double floorNumber;
-						private String spotName;
-						private double timeNumber;
-						private double timeNumber2;
-			
-						public void information (String Name, double floorNumber, double timeNumber, double timeNumber2)
-						{
+						}
+						public void setName(String Name){
 							this.Name = Name;
+						}
+						public String getName(){
+							return this.Name;
+						}
+						public void setfloorNumber(double floorNumber){
 							this.floorNumber = floorNumber;
-							this.timeNumber = timeNumber;
-							this.timeNumber2 = timeNumber2;
 						}
-				public information() {
-			
-				}
-				public void setName(String Name){
-					this.Name = Name;
-				}
-				public String getName(){
-					return this.Name;
-				}
-				public void setfloorNumber(double floorNumber){
-					this.floorNumber = floorNumber;
-				}
-				public double getfloorNumber(){
-					return this.floorNumber;
-				}
-				}
-			/*
-				break;
-
-				case 2:
-				//Cancel reservation through spot location?
-					String name, double floorNumber, slotNumber;
-					while (input.hasNext() && !found){
-				try {
-					if (information.equals(name)) && (information.equals(floorNumber) && (informatiion.equals(slotNumber)))
-					{
-						found = true;
-					}
-				}
-				System.out.println ("Cancelation successful")
-			}catch(Exception e){
-
-				System.out.println( "Cancelation unsuccessful")
-			}
-				break;
-
-				case 3:
-				//Quick Reserve. (Pseudo code that needs to be edited after the previous errors in this class are fixed)
-				 
-				int garageNumber, floorNumber, slotNumber;
-				boolean foundSlot = false;
-				while(!foundSlot){
-						for(int i=1; i<=garageList.size(); i++) {
-							for(int j=1; j<=garageList.get(0).getNumberOfFloors();j++) {			
-								for(int k=0; k<numberOSlots; k++) {
-									boolean slotAvailability = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getAvailability();
-									if (slotAvailability){
-										slotNumber = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getSlotNumber();
-										garageNumber=i;
-										floorNumber=j;
-										foundSlot=true;
-
-									}
-									if(foundSlot){
-										break;
-									}
-								}
-								if(foundSlot){
-										break;
-								}
-							}
-							if(foundSlot){
-										break;
-							}
+						public double getfloorNumber(){
+							return this.floorNumber;
 						}
-						
-						//Now do the reservation with garageNumber, floorNumber, slotNumber
-						
-				}
-				
-				 
-				default:
-					
-			 */
-			
-				}
-				
-		}while(choice !=4);
-	}
-}
+						}
+*/
+
+/*Adapt to main? How to take in main choice into reservation class choice
+  Choice M or R would work with do case system in reservation?
+  Delete case statements and just make methods?*/
