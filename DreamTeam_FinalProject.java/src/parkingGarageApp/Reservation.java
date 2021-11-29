96da79d0b39fb74be2d68d23ee6ce03c6308769c
Whonald <ronaldher2@csus.edu>
2021-11-28 16:55:46 -0800

    Cleanup
    Just cleanup.

--- DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
+++ DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
@@ -88,7 +88,7 @@ public class Reservation {
 
 												}
 												if(foundSlot){
-													
+
 													break;
 												}
 											}
@@ -116,57 +116,6 @@ public class Reservation {
 	}
 
 }
-	
-/*
-	public void checkSpot() {
-		
-		System.out.println("Input the floor number that you would like to book from. \n");
-		int floorNumber = key.nextInt();
-		System.out.println("Input parking space # that you would like to book.\n");
-		int spotNumber = key.nextInt();
-		
-		checkspot.getSpotName(String spotName); //something like this... fix later
-		
-		if (spotName == null) {							//getAvailability?
-			System.out.println("Spot is empty");
-		}
-		else {
-			reserve.checkSpace(spotName);			//make a class to check spot
-			System.out.println("Spot is currently filled by, " + spotName);			//loop back?
-		}
-	}
-
-	public class information {
-								private String Name;
-								private double floorNumber;
-								private String spotName;
-								private double timeNumber;
-								private double timeNumber2;
-					
-								public void information (String Name, double floorNumber, double timeNumber, double timeNumber2)
-								{
-									this.Name = Name;
-									this.floorNumber = floorNumber;
-									this.timeNumber = timeNumber;
-									this.timeNumber2 = timeNumber2;
-								}
-						public information() {
-					
-						}
-						public void setName(String Name){
-							this.Name = Name;
-						}
-						public String getName(){
-							return this.Name;
-						}
-						public void setfloorNumber(double floorNumber){
-							this.floorNumber = floorNumber;
-						}
-						public double getfloorNumber(){
-							return this.floorNumber;
-						}
-						}
-*/
 
 /*Adapt to main? How to take in main choice into reservation class choice
   Choice M or R would work with do case system in reservation?
