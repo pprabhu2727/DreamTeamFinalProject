cf92f4c7ca400807c27de7ac0cb5543de32e5a50
Bryan Burch <bburch@csus.edu>
2021-11-29 13:29:03 -0800

    Starting work on tests for vehicle class

---
+++ DreamTeam_FinalProject.java/src/tests/VehicleTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import parkingGarageApp.Vehicle;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VehicleTest {
+
+	int    year;
+	String make;
+	String model;
+	String license;
+	String permit;
+	
+	Vehicle v;
+	
+	
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
