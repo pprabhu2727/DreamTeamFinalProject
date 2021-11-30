072c4db179c9dfdd062e5539c8876f707241419f
ffine987 <ffine@csus.edu>
2021-11-30 08:23:50 -0800

    Display slot numbers

--- DreamTeam_FinalProject.java/src/parkingGarageApp/GarageFloorPrinter.java
+++ DreamTeam_FinalProject.java/src/parkingGarageApp/GarageFloorPrinter.java
@@ -3,24 +3,20 @@ import java.util.*;
 
 public class GarageFloorPrinter {
 	
-	// 12 x 12 grid with 48 spaces, 2 stairs, 1 elevator.
-	// the sides display row and column numbers for spaces
-	// Serves as a template for the structure of the map.
-	private static char map[][] = { 
-			{ ' ', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' },
-			{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
-			{ '0', ' ', '@', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'E' },
-			{ '1', ' ', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*' },
-			{ '2', ' ', 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
-			{ '3', ' ', 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
-			{ '4', ' ', 'A', '*', '*', '*', '*', '*', '*', '*', '*', 'A' },
-			{ '5', ' ', 'A', '*', '*', '*', '*', '*', '*', '*', '*', 'A' },
-			{ '6', ' ', 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
-			{ '7', ' ', 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
-			{ '8', ' ', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*' },
-			{ '9', ' ', '*', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', '@' },
-			};
+	static int slotNumber[] = new int[48];
 	
+	private static char map[][] = { 
+			{ '@', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'E' },
+			{ '*', '*', '*', '*', '*', '*', '*', '*', '*', '*' },
+			{ 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
+			{ 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
+			{ 'A', '*', '*', '*', '*', '*', '*', '*', '*', 'A' },
+			{ 'A', '*', '*', '*', '*', '*', '*', '*', '*', 'A' },
+			{ 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
+			{ 'A', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', 'A' },
+			{ '*', '*', '*', '*', '*', '*', '*', '*', '*', '*' },
+			{ '*', '*', 'A', 'A', 'A', 'A', 'A', 'A', '*', '@' },
+			};
 	
 	private static int createFloorMap(int garageNum, int floorNum) {
 		int numSlotsAvailable = 0;
@@ -29,8 +25,8 @@ public class GarageFloorPrinter {
 		GarageFloor floor = CampusParking.getGarageList().get(garageNum).getFloorList().get(floorNum);
 		
 		// Modifying map template with correct availability
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
 				if (map[i][j] == 'A' || map[i][j] == 'R') {
 					boolean isSlotAvailable = floor.getSlots().get(i).getAvailability();
 					
@@ -51,19 +47,33 @@ public class GarageFloorPrinter {
 		// Garage and floor start at index 0 internally
 		int numAvailable = createFloorMap(garageNum - 1, floorNum - 1);
 		
+		for (int i = 0; i < slotNumber.length; i++) {
+			slotNumber[i] = i+1;
+		}
+		
 		System.out.println("----------------------------------------");
 		System.out.print("[Garage " + garageNum + ", Floor " + floorNum +"]");
 		System.out.println("  [Available Spaces: " + numAvailable + "]\n");
 		
-		for (int i = 0; i < 12; i++) {
-			for (int j = 0; j < 12; j++) {
-				System.out.print(map[i][j] + "  ");
+		int k = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (map[i][j] == 'A') {
+					System.out.print(slotNumber[k] + " ");
+					if (k < 9) {
+						System.out.print(" ");
+					}
+					k++;
+				}
+				else {
+					System.out.print(map[i][j] + "  ");
+				}
 			}
 			System.out.println();
 		}
 		
 		System.out.println("\n[Key]");
-		System.out.println("[A = Available Slot]");
+		//System.out.println("[A = Available Slot]");
 		System.out.println("[R = Reserved Slot]");
 		//System.out.println("[H = Handicapped Slot]");
 		//System.out.println("[S = Staff-Only Slot]");
