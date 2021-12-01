33fd7734366eff9456f22ed10a8f86bd6bda080d
Whonald <ronaldher2@csus.edu>
2021-12-01 00:25:03 -0800

    Update Reservation
    Finished working on case fixes. Should be working. Need to test and fix errors.

--- DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
+++ DreamTeam_FinalProject.java/src/parkingGarageApp/Reservation.java
@@ -37,15 +37,15 @@ public class Reservation {
 					switch(choice){
 						case 1:
 
+
 							System.out.println(" Please enter a garage number you'd like to reserve from: ");
 							gChoice = g.nextInt();
 							System.out.println(" Please enter a floor number you'd like to reserve from: ");
 							fChoice = f.nextInt();
 							System.out.println(" Please enter a slot number you'd like to reserve: ");
 							sChoice = s.nextInt();
-							ParkingSlot.ParkingSlot(gChoice, fChoice, sChoice);	//Is there a way to change the method to work like this? and make getAvailability to check exact spot?
-
-							if(ParkingSlot.getAvailability == true)
+						
+							if(garageList.get(gChoice).getFloorList().get(fChoice).getSlots().get(sChoice).getAvailability == true)
 							{
 								ParkingSlot.changeAvailability;
 								ParkingSlot.setUser;
@@ -56,35 +56,19 @@ public class Reservation {
 
 						case 2:
 
-							/* Idea?
+							//If users doesn't hold reservation value, use iteration loop from case 3 to match names to clear user and change availability.
 							if(Users.getReservation == true)
 							{
+								Users.getReservation = false;
+								ParkingSlot.changeAvailability;
 								ParkingSlot.clearUser;
+							
 							}
-		
-							*/
-
-							/*Cancel reservation through spot location?
-								String name, int floorNumber, slotNumber;
-								while (input.hasNext() && !found){
-							try {
-								if (information.equals(name)) && (information.equals(floorNumber) && (informatiion.equals(slotNumber)))
-								{
-									found = true;
-								}
-							}
-							System.out.println ("Cancelation successful")
-							}catch(Exception e){
-
-								System.out.println( "Cancelation unsuccessful");
-							}
-							*/
 
 							break;
 
 						case 3:
-							//Quick Reserve. (Pseudo code that needs to be edited after the previous errors in this class are fixed)
-							
+
 							int garageNumber, floorNumber, slotNumber;
 							boolean foundSlot = false;
 							while(!foundSlot){
@@ -92,43 +76,28 @@ public class Reservation {
 										for(int j=1; j<=garageList.get(0).getNumberOfFloors();j++) {			
 											for(int k=0; k<numberOSlots; k++) {
 												boolean slotAvailability = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getAvailability();
-												if (slotAvailability){
-													slotNumber = garageList.get(i).getFloorList().get(j).getSlots().get(k+1).getSlotNumber();
-													garageNumber=i;
-													floorNumber=j;
-													foundSlot=true;
-
+												if (slotAvailability == true){
+													ParkingSlot.changeAvailability;
+													ParkingSlot.setUser;
+													reservation = true;
 												}
-												if(foundSlot){
-
-													break;
-												}
+							
 											}
-											if(foundSlot){
-													break;
-											}
+								
 										}
-										if(foundSlot){
-													break;
-										}
+								
 									}
-									
-									//Now do the reservation with garageNumber, floorNumber, slotNumber
-									
 							}
+							break;
 						
 						
-						//default:
+					
 							
 				
 				
 			}
 		
-		}while(choice != 4);
+		}while(choice == 4);
 	}
 
 }
-
-/*Adapt to main? How to take in main choice into reservation class choice
-  Choice M or R would work with do case system in reservation?
-  Delete case statements and just make methods?*/
