f54ee29570352851cac41fd74076ad1b42c00d5d
pprabhu2727 <pprabhu@csus.edu>
2021-12-05 11:59:34 -0800

    CampusParkingTest 100% coverage

--- DreamTeam_FinalProject.java/src/tests/CampusParkingTest.java
+++ DreamTeam_FinalProject.java/src/tests/CampusParkingTest.java
@@ -1,15 +1,30 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import parkingGarageApp.CampusParking;
+import parkingGarageApp.ParkingGarage;
 
 import org.junit.Test;
 
 public class CampusParkingTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void generalTest() {
+		CampusParking x = CampusParking.createInstance();
+	}
+	@Test
+	public void singletonTest() {
+		CampusParking x = CampusParking.createInstance();
+		CampusParking y = CampusParking.createInstance();
+		assertEquals(x,y);
+	}
+	@Test
+	public void getGarageListTest() {
+		ArrayList<ParkingGarage> x = CampusParking.getGarageList();
+		assertEquals(x,CampusParking.getGarageList());
 	}
 
 }
