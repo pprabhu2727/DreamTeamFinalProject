db5f5f7ced809755b68d6852ee37f8598e686694
Rthandi01 <91033806+Rthandi01@users.noreply.github.com>
2021-12-06 13:53:27 -0800

    100% Coverage for Reservation class

--- DreamTeam_FinalProject.java/src/tests/ReservationTest.java
+++ DreamTeam_FinalProject.java/src/tests/ReservationTest.java
@@ -2,7 +2,10 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import parkingGarageApp.CampusParking;
+import parkingGarageApp.ParkingGarage;
 import parkingGarageApp.ParkingSlot;
 import parkingGarageApp.Reservation;
 import parkingGarageApp.Users;
@@ -19,13 +22,25 @@ public class ReservationTest {
 	}
 	@Test
 	public void makeReservTest() {
-		Reservation x = new Reservation(1,1,1);
+		int garageNumber = 1;
+		int floorNumber  = 1;
+		int slotNumber   = 1;
+		int i;
+		int j;
+		int k;
+		Boolean reservation = false;
+		Reservation x = new Reservation(garageNumber,floorNumber,slotNumber);
 		Users u = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
 		Users u2 = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
+		u.setReservation(x);
+		assertEquals(x,u.getReservation());
+		CampusParking.createInstance();
+		ParkingSlot ps= new ParkingSlot(slotNumber);
+		assertEquals(true,ps.getAvailability());
+		ps.changeAvailability();
+		assertEquals(false,ps.getAvailability());
+		ps.setUser(u);
 		x.makeReserv(u);
-		ParkingSlot ps= new ParkingSlot(1);
-		int i = CampusParking.getGarageList().size();
-		
 	
 	}
 	@Test
@@ -33,11 +48,11 @@ public class ReservationTest {
 		Reservation x = new Reservation(1,1,1);
 		Users u = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
 		x.makeReserv(u); 
-		x.setReser("yes");
-		assertEquals("yes",x.getReser());
+		x.setReser("Yes");
+		assertEquals("Yes",x.getReser());
 		x.setReservation(true);
-		assertEquals(true,x.getReservation());
-		
+		x.getReservation();
+	
 	}
 	@Test
 	public void toStringTest() {
