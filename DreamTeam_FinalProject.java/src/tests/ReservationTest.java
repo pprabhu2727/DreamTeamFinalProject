f5d14c09a19bb5d7d7d20d9ca7a8496258773b61
Rthandi01 <91033806+Rthandi01@users.noreply.github.com>
2021-12-05 23:54:51 -0800

    67% coverage overall coverage makeReserv is 41%

--- DreamTeam_FinalProject.java/src/tests/ReservationTest.java
+++ DreamTeam_FinalProject.java/src/tests/ReservationTest.java
@@ -1,6 +1,9 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import parkingGarageApp.CampusParking;
+import parkingGarageApp.ParkingSlot;
 import parkingGarageApp.Reservation;
 import parkingGarageApp.Users;
 
@@ -19,18 +22,30 @@ public class ReservationTest {
 		Reservation x = new Reservation(1,1,1);
 		Users u = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
 		Users u2 = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
-		x.makeReserv(u); 
+		x.makeReserv(u);
+		ParkingSlot ps= new ParkingSlot(1);
+		int i = CampusParking.getGarageList().size();
+		
+	
 	}
 	@Test
 	public void getAndsetTest() {
 		Reservation x = new Reservation(1,1,1);
 		Users u = new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber");
 		x.makeReserv(u); 
-		x.setReser("Yes");
-		assertEquals("Yes",x.getReser());
+		x.setReser("yes");
+		assertEquals("yes",x.getReser());
 		x.setReservation(true);
 		assertEquals(true,x.getReservation());
+		
 	}
-	
-
+	@Test
+	public void toStringTest() {
+		int garageNumber = 1;
+		int floorNumber  = 1;
+		int slotNumber   = 1;
+		Reservation r = new Reservation(1,1,1);
+		String s = "Your reservation has been set for Garage:" +garageNumber +" Floor:" +floorNumber +" and Slot number:" +slotNumber;
+		assertEquals(s,r.toString());
+	}
 }
