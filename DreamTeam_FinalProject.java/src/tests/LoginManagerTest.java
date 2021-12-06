a6cf07aa74a1e65b5741daddac5ee61f6c71007f
Rthandi01 <91033806+Rthandi01@users.noreply.github.com>
2021-12-06 00:21:10 -0800

    LoginManager Coverage login case and ReadFromFile remaining

--- DreamTeam_FinalProject.java/src/tests/LoginManagerTest.java
+++ DreamTeam_FinalProject.java/src/tests/LoginManagerTest.java
@@ -1,15 +1,36 @@
 package tests;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import parkingGarageApp.LoginManager;
+import parkingGarageApp.Reservation;
+import parkingGarageApp.Users;
+import parkingGarageApp.Vehicle;
 
 import org.junit.Test;
 
 public class LoginManagerTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
+	@Test
+    public void loginTest() throws Exception {
+		ArrayList<Users> listOfUsers = new ArrayList<Users>();
+		listOfUsers.add(new Users("String name", "String Email", "String Address", "String Username", "String Password", "String PhoneNumber",new Vehicle("String Year", "String Make", "String Model", "String License", "String Permit"),new Reservation(1,1,1)));
+		Users loggedInUser = null;
+    	String greeting = "Welcome. Please login or register to continue.";
+    	Scanner in = new Scanner(System.in);
+    	LoginManager.login(in);
+   
+    	
+    }
+	@Test
+    public void readAccountTest() throws Exception {
+		File f = new File("Output.txt");
+		LoginManager.readAccountsFrom("Output.txt");
+    	
+    }
 }
